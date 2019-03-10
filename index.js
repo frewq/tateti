72d3dcce94294526eb5f9fb96785f5783af13eac
frewq <fabi85_592@msn.com>
2019-03-10 16:45:44 -0300

    Version 1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //TATETI
 
 let turno = true;
+let jugar = true;
 let arr = [];
 
 document.querySelectorAll(".numero").forEach(i => i.addEventListener("click", (e) => {
@@ -11,7 +12,7 @@ document.querySelectorAll(".numero").forEach(i => i.addEventListener("click", (e
 function tateti(ev) {
   switch (turno) {
     case true:
-      if (ev.target.innerHTML != "X" && ev.target.innerHTML != "O") {
+      if (ev.target.innerHTML != "X" && ev.target.innerHTML != "O" && jugar == true) {
         ev.target.innerHTML = "X";
         turno = false;
         pintar(ev, 'X');
@@ -19,7 +20,7 @@ function tateti(ev) {
       }
       break;
     case false:
-      if (ev.target.innerHTML != "X" && ev.target.innerHTML != "O") {
+      if (ev.target.innerHTML != "X" && ev.target.innerHTML != "O" && jugar == true) {
         ev.target.innerHTML = "O";
         turno = true;
         pintar(ev, 'O');
@@ -31,7 +32,7 @@ function tateti(ev) {
 
 function preguntarSiGano(ev, XO) {
 
-  arr[ev.target.attributes.valor.value - 1] = XO;
+  arr[ev.target.attributes.valor.value] = XO;
 
   if (arr[0] == `${XO}` && (arr[1] == `${XO}`) && (arr[2] == `${XO}`)) gano(XO);
   if (arr[3] == `${XO}` && (arr[4] == `${XO}`) && (arr[5] == `${XO}`)) gano(XO);
@@ -49,5 +50,22 @@ function pintar(ev, color) {
 }
 
 function gano(XO) {
+  jugar = false;
   document.getElementById("resultado").innerHTML = `${XO} Es el ganador`;
+  document.getElementById("resultado").innerHTML += "<button id='reiniciar'>Reiniciar</button>";
+  // document.getElementsByTagName("body").innerHTML += "<button id='reiniciar'>Reiniciar</button>";
+  document.getElementById("reiniciar").addEventListener("click", () => reiniciar());
+}
+
+function reiniciar() {
+
+  //arreglar i
+  for (let i = 0; i < 9; i++) {
+    document.getElementById(i).innerHTML = "+";
+    document.getElementById(i).setAttribute("color", "");
+    arr[i] = '';
+  }
+
+  document.getElementById("resultado").innerHTML = "";
+  jugar = true;
 }
